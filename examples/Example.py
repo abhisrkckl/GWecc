fc63bc251ea7967d75cafc0cd04229b3a3fd15e1
Abhimanyu S <abhisrkckl@gmail.com>
2019-08-01 10:45:06 +1000

    updated examples

--- examples/Example.py
+++ examples/Example.py
@@ -9,12 +9,12 @@ ns = 1e-9
 
 toas = year*np.linspace(0,15,1000)
 
-RA_psr = 0
-DEC_psr = 0
+RA_psr = 0.65
+DEC_psr = -1.12
 D_psr = 700
 	 
 RA_GW = np.pi/3
-DEC_GW = np.pi/3
+DEC_GW = np.pi/3.5
 log10_D_GW = 9
 
 psi = 0
@@ -24,7 +24,7 @@ log10_M = 10
 q = 1
 
 log10_f0_GW = -8
-e0 = 0.5
+e0 = 0.01
 gamma0 = 0
 l0 = 0
 t0 = 0
@@ -35,7 +35,7 @@ residuals = GWecc.eccentric_cw_delay(toas,
 	     	       		     psi, i,
 	     	       		     log10_M, q,
 	     	       		     log10_f0_GW, e0, gamma0, l0, t0,
-	     	       		     psrTerm=True)
+	     	       		     psrTerm=False)
 
 plt.plot(toas/year, np.asarray(residuals)/ns)
 plt.xlabel("t (yr)")
--- examples/Example2.py
+++ examples/Example2.py
@@ -9,11 +9,12 @@ ns = 1e-9
 
 toas = year*np.linspace(0,15,1000)
 
-RA_P = 0.65
+"""
+RA_P = 1.65
 DEC_P = -1.12
 D_P = 700
 	 
-RA_GW = np.pi/3
+RA_GW = 3.1
 DEC_GW = -np.pi/3.5
 D_GW = 1e9
 
@@ -29,6 +30,16 @@ e0 = 0.5
 gamma0 = 0
 l0 = 0
 t0 = 0
+"""
+
+M = 1900000000.0
+q= 1
+Omega= 1.5707963267948966
+i= 0
+t0, n0, e0, l0, gamma0 = ( 0, 1.9378467723779355e-07, 0.01, 0, 0 )
+D_GW, RA_GW, DEC_GW = ( 92000000.0, 0.6247850534696544, 0.7503528489227123 )
+D_P, RA_P,  DEC_P = ( 700, 0.0, -1.5707963267948966 )
+Pb = 2*np.pi/n0
 
 residuals = GWecc.GWecc.EccentricResiduals(M, q,
                                            Omega, i,
@@ -47,7 +58,7 @@ Rp,Rx = GWecc.GWecc.EccentricResiduals_px(M, q,
                                           toas)
 
 cosmu,Fp,Fx = GWecc.GWecc.AntennaPattern(RA_GW, DEC_GW, RA_P, DEC_P)
-residuals1 = np.array(Rp)*Fp + np.array(Rx)*Fx;
+residuals1 = -(np.array(Rp)*Fp + np.array(Rx)*Fx);
 
 """residuals = GWecc.eccentric_cw_delay(toas, 
 	     	       		     RA_psr, DEC_psr, D_psr,
