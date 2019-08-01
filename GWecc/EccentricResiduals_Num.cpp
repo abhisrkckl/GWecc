c8f9f8cdd680d9ad86a318336f5e348034553998
Abhimanyu S <abhisrkckl@gmail.com>
2019-08-01 10:43:52 +1000

    --

--- GWecc/EccentricResiduals_Num.cpp
+++ GWecc/EccentricResiduals_Num.cpp
@@ -24,6 +24,7 @@ Signal1D EccentricResiduals_Num(const BinaryMass &bin_mass,
 	else if(residuals_terms==ResidualsTerms::Pulsar){
 		
 		const auto delay = -psr_pos.DL*(1-cosmu);
+		std::cout<<"delay = "<<delay<<'\n';
 		const auto bin_psrterm = solve_orbit_equations(bin_mass, bin_init, delay);
 		
 		return EccentricResiduals_fn_Num(bin_mass, bin_psrterm, Fp, Fx, bin_pos.DL, ts + delay);
@@ -130,26 +131,26 @@ double EccentricResiduals_fn_pt(double t, void *_params){
 						+ (ci*ci+1)*(2*e*e - ecu*ecu + ecu - 2)	*c2phi
 						+ (si*si)  *(1-ecu)*ecu
                            		     ),
-                   h_mq_B  = 1./ipow(1-ecu,2) * (	 	    (2*OTS*esu)		*c2phi
-                           			+ 	    (2*e*e - ecu*ecu + ecu - 2) *s2phi
-					     )*2*ci;
+               h_mq_B  = 1./ipow(1-ecu,2) * (	 	    (2*OTS*esu)		*c2phi
+                       			+ 	    (2*e*e - ecu*ecu + ecu - 2) *s2phi
+				                    )*2*ci;
 	
 	const auto h_cq_A  = 1./ipow(1-ecu,3) * (   (1-ecu)*(-5-ipow(ci,2) + (-2+6*ipow(ci,2))*ecu)*OTS		*c1phi
 						  +  2*(1-3*ipow(ci,2))*ipow(1-ecu,2)*esu			*s1phi 
 						  + (1+ipow(ci,2))*OTS*(9-8*ipow(e,2)-7*ecu+6*ipow(ecu,2))	*c3phi 
 						  + (1+ipow(ci,2))*2*esu*(5-4*ipow(e,2)-2*ecu+ipow(ecu,2))	*s3phi
-						)*delta*si/4,
-		   h_cq_B  = 1./ipow(1-ecu,3) * (   2*ipow(1-ecu,2)*esu						*c1phi 
-		   				  + (1-ecu)*(-3+2*ecu)*OTS					*s1phi		   
-		   				  + 2*esu*(-5+4*ipow(e,2)+2*ecu-ipow(ecu,2)) 			*c3phi
-		   				  + (9-8*ipow(e,2)-7*ecu+6*ipow(ecu,2))*OTS			*s3phi
-		   				)*delta*ci*si/2;
+						  )*delta*si/4,
+		       h_cq_B  = 1./ipow(1-ecu,3) * (   2*ipow(1-ecu,2)*esu						*c1phi 
+		       				  + (1-ecu)*(-3+2*ecu)*OTS					*s1phi		   
+		       				  + 2*esu*(-5+4*ipow(e,2)+2*ecu-ipow(ecu,2)) 			*c3phi
+		       				  + (9-8*ipow(e,2)-7*ecu+6*ipow(ecu,2))*OTS			*s3phi
+		       				)*delta*ci*si/2;
 	
 	const auto hA	= h_mq_A + sqrtx*h_cq_A,
-		   hB	= h_mq_B + sqrtx*h_cq_B;
+		       hB	= h_mq_B + sqrtx*h_cq_B;
 	
 	const auto hp 	= c2Om*hA - s2Om*hB,
-		   hx 	= c2Om*hB + s2Om*hA;
+		       hx 	= c2Om*hB + s2Om*hA;
 	
 	return H0*(Fp*hp + Fx*hx);
 }
@@ -177,10 +178,10 @@ std::tuple<Signal1D, Signal1D> EccentricResiduals_px_Num(const BinaryMass &bin_m
 
 
         gsl_function EccentricResiduals_gsl_func_p {&EccentricResiduals_fn_pt, &params_p},
-		     EccentricResiduals_gsl_func_x {&EccentricResiduals_fn_pt, &params_x};
+		             EccentricResiduals_gsl_func_x {&EccentricResiduals_fn_pt, &params_x};
 
-	Signal1D Rp = qag_integrator.eval_noerr(EccentricResiduals_gsl_func_p, ts[0], ts),
-		 Rx = qag_integrator.eval_noerr(EccentricResiduals_gsl_func_x, ts[0], ts);
+	    Signal1D Rp = qag_integrator.eval_noerr(EccentricResiduals_gsl_func_p, ts[0], ts),
+		         Rx = qag_integrator.eval_noerr(EccentricResiduals_gsl_func_x, ts[0], ts);
         return std::make_tuple(Rp,Rx);
 
 }
