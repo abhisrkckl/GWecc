e269b7695a52a833f1c74e205860ec81f4a7193b
Abhimanyu S <abhisrkckl@gmail.com>
2019-08-06 10:02:24 +1000

    Added redshift

--- GWecc/EccentricResiduals_py.hpp
+++ GWecc/EccentricResiduals_py.hpp
@@ -9,26 +9,29 @@
  * Computes R(t)
  */
 std::vector<double> EccentricResiduals( const double M, const double q,
-					const double Omega, const double i, 
-					const double t0, const double n0, const double e0, const double l0, const double gamma0,
-					const double D_GW, const double RA_GW, const double DEC_GW, 
-					const double D_P,  const double RA_P,  const double DEC_P, 
-					const ResidualsMethod residuals_method,
-					const ResidualsTerms residuals_terms,
-					const std::vector<double> ts);
+                    const double Omega, const double i, 
+                    const double t0, const double n0, const double e0, const double l0, const double gamma0,
+                    const double D_GW, const double RA_GW, const double DEC_GW, 
+                    const double D_P,  const double RA_P,  const double DEC_P, 
+                    const double z,
+                    const ResidualsMethod residuals_method,
+                    const ResidualsTerms residuals_terms,
+                    const std::vector<double> ts);
 
 std::vector<std::vector<double> > EccentricResiduals_px(const double M, const double q,
                                                         const double Omega, const double i,
                                                         const double t0, const double n0, const double e0, const double l0, const double gamma0,
-                                                        const double DGW,
+                                                        const double DGW, const double z,
                                                         const ResidualsMethod residuals_method,
                                                         const std::vector<double> _ts);
 
-std::vector<std::vector<double> > EccentricWaveform_px(	const double M, const double q,
+/*
+std::vector<std::vector<double> > EccentricWaveform_px( const double M, const double q,
                                                         const double Omega, const double i,
                                                         const double t0, const double n0, const double e0, const double l0, const double gamma0,
-                                                        const double DGW,
+                                                        const double DGW, const double z,
                                                         const std::vector<double> _ts);
+*/
 
 BinaryState solve_orbit_equations(const BinaryMass &bin_mass, const BinaryState &bin_init, const double delay);
 
