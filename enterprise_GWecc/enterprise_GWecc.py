e2b35392b68559430ebedef868e3a874b3d768e1
Abhimanyu S <abhisrkckl@gmail.com>
2021-05-05 17:52:16 +0530

    Changed the arguments of eccentric_cw_delay(..) to better resemble compute_eccentric_residuals(..) in enterprise_extensions

--- enterprise_GWecc/enterprise_GWecc.py
+++ enterprise_GWecc/enterprise_GWecc.py
@@ -7,10 +7,12 @@ year_to_s   = 365.25*24*3600
 @signal_base.function
 def eccentric_cw_delay(toas, 
                        theta, phi, pdist, 
-                       cos_gwtheta, gwphi, log10_dist,
+                       cos_gwtheta, gwphi, 
+                       log10_dist,
+                       log10_h,
                        psi, cos_inc,
                        log10_M, q,
-                       log10_f0_GW, e0, gamma0, l0, tref,
+                       log10_F, e0, gamma0, l0, tref,
                        z,
                        p_dist=1.0,
                        psrTerm=False,
@@ -29,7 +31,10 @@ def eccentric_cw_delay(toas,
     
     cos_gwtheta is  Cos of Polar angle of GW source     in rad      = cos(pi/2 - DEC_GW)     
     gwphi       is  Azimithal angle of GW source        in rad      = RA_GW
-    log10_dist  is  log10 distance to GW source         in Mpc      = log10(D_GW)
+    
+    log10_dist  is  log10 distance to GW source         in Mpc      = log10(D_GW)           (One and only one of log10_dist and log10_h should be used.) 
+    
+    log10_h     is  log10 GW amplitude at tref                      = log10(H0)             (One and only one of log10_dist and log10_h should be used.) 
     
     psi         is  Polarization angle                  in rad      
     cos_inc     is  Cos of Inclination of GW source     in rad      = cos(i)
@@ -37,7 +42,7 @@ def eccentric_cw_delay(toas,
     log10_M     is  log10 Total Mass of GW src          in MSun     = log10(M)              (Not to be confused with the chirp mass.)
     q           is  Mass ratio of GW source                         = m1/m2
     
-    log10_f0_GW is  log10 GW frequency at t=tref        in Hz       = log10(2/Pb0)
+    log10_F     is  log10 GW frequency at t=tref        in Hz       = log10(2/Pb0)
     e0          is  Eccentricity at t=tref
     gamma0      is  Periastron angle at t=tref          in rad
     l0          is  Mean anomaly at t=tref              in rad                              (Should be set to zero if tref is a free parameter.)
@@ -71,14 +76,23 @@ def eccentric_cw_delay(toas,
     gwtheta = np.arccos(cos_gwtheta)
     DEC_GW = np.pi/2 - gwtheta
     RA_GW  = gwphi
-    
-    D_GW = 1e6 * (10.**log10_dist)   
+        
     M = 10.**log10_M
     
     i = np.arccos(cos_inc)
     
-    n0 = np.pi*(10.**log10_f0_GW)    # GW frequency is twice the orbital frequency.
+    n0 = np.pi*(10.**log10_F)    # GW frequency is twice the orbital frequency.
     Pb0 = 2*np.pi/n0 / year_to_s
+    
+    if (log10_dist is None and log10_h is None) or (log10_dist is not None and log10_h is not None):
+        raise ValueError("One and only one of log10_dist and log10_h should be used. The other should be set to None.")
+    elif log10_dist is not None:
+        D_GW = 1e6 * (10.**log10_dist)
+    else:
+        H0 = 10.**log10_h
+        x0 = (M*n0)**(2/3)
+        eta = q/(1+q)**2
+        D_GW = M*eta*x/H0
 
     residuals_method = ResidualsMethod_Num
     residuals_terms = ResidualsTerms_Both if psrTerm else ResidualsTerms_Earth
@@ -100,7 +114,7 @@ def Fe_statistic_funcs(toas,
                        theta, phi, 
                        cos_gwtheta, gwphi,
                        log10_M, q,
-                       log10_f0_GW, e0, gamma0, l0, tref,
+                       log10_F, e0, gamma0, l0, tref,
                        z):
 
     M = 10.**log10_M
@@ -115,7 +129,7 @@ def Fe_statistic_funcs(toas,
     DEC_GW = np.pi/2 - gwtheta
     RA_GW  = gwphi
     
-    n0 = np.pi*(10.**log10_f0_GW)    # GW frequency is twice the orbital frequency.
+    n0 = np.pi*(10.**log10_F)    # GW frequency is twice the orbital frequency.
     Pb0 = 2*np.pi/n0 / year_to_s
                            
     return GWecc.GWecc.FeStatFuncs(M, q, tref, Pb0, e0, l0, gamma0,
