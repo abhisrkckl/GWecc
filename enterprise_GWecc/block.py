3b362467a755eb28532f91cfdfea68c260d06bf8
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-07-29 14:56:00 -0500

    blacken

--- enterprise_GWecc/block.py
+++ enterprise_GWecc/block.py
@@ -5,50 +5,67 @@ from enterprise.signals.parameter import Uniform, Constant
 from enterprise_GWecc import eccentric_cw_delay_Planck18
 from enterprise_extensions.deterministic import CWSignal
 
-def gwecc_block(tref, skyloc=None, log10_F=None, ecc=None, redshift=None, psrTerm=False, name='gwecc'):
+
+def gwecc_block(
+    tref,
+    skyloc=None,
+    log10_F=None,
+    ecc=None,
+    redshift=None,
+    psrTerm=False,
+    name="gwecc",
+):
     """enterprise_extensios-style convinience function for creating a Signal for GWecc"""
     if skyloc is None:
         cos_gwtheta = Uniform(-1, 1)(f"{name}_cos_gwtheta")
-        gwphi = Uniform(0, 2*np.pi)(f"{name}_gwphi")
+        gwphi = Uniform(0, 2 * np.pi)(f"{name}_gwphi")
     else:
         cos_gwtheta = Constant(skyloc[0])(f"{name}_cos_gwtheta")
         gwphi = Constant(skyloc[1])(f"{name}_gwphi")
 
     if log10_F is None:
-        log10_F = Uniform(-9.0, -7.0)(f'{name}_log10_F')
+        log10_F = Uniform(-9.0, -7.0)(f"{name}_log10_F")
     else:
-        log10_F = Constant(log10_F)(f'{name}_log10_F')
+        log10_F = Constant(log10_F)(f"{name}_log10_F")
 
     if ecc is None:
-        e0 = Uniform(0.0, 0.8)(f'{name}_e0')
+        e0 = Uniform(0.0, 0.8)(f"{name}_e0")
     else:
-        e0 = Constant(ecc)(f'{name}_e0')
+        e0 = Constant(ecc)(f"{name}_e0")
 
-    psi = Uniform(-np.pi/2, np.pi)(f'{name}_psi')
-    cos_inc = Uniform(-1, 1)(f'{name}_cos_inc')
+    psi = Uniform(-np.pi / 2, np.pi)(f"{name}_psi")
+    cos_inc = Uniform(-1, 1)(f"{name}_cos_inc")
 
-    log10_M = Uniform(6.0, 9.1)(f'{name}_log10_M')
-    q = Uniform(0.01, 1)(f'{name}_q')
+    log10_M = Uniform(6.0, 9.1)(f"{name}_log10_M")
+    q = Uniform(0.01, 1)(f"{name}_q")
 
-    gamma0 = Uniform(-np.pi/2, np.pi)(f'{name}_psi')
-    l0 = Uniform(-np.pi, np.pi)(f'{name}_psi')
-    
-    tref = Constant(tref)(f'{name}_tref')
-    
+    gamma0 = Uniform(-np.pi / 2, np.pi)(f"{name}_psi")
+    l0 = Uniform(-np.pi, np.pi)(f"{name}_psi")
+
+    tref = Constant(tref)(f"{name}_tref")
+
     if redshift is None:
-        z = Uniform(0, 1)(f'{name}_q')
+        z = Uniform(0, 1)(f"{name}_q")
     else:
-        z = Constant(redshift)(f'{name}_q')
-    
-    wf = eccentric_cw_delay_Planck18(cos_gwtheta=cos_gwtheta, gwphi=gwphi, 
-                                     psi=psi, cos_inc=cos_inc,
-                                     log10_M=log10_M, q=q,
-                                     log10_F=log10_F, e0=e0, gamma0=gamma0, l0=l0, tref=tref,
-                                     z=z,
-                                     psrTerm=psrTerm)
-    
+        z = Constant(redshift)(f"{name}_q")
+
+    wf = eccentric_cw_delay_Planck18(
+        cos_gwtheta=cos_gwtheta,
+        gwphi=gwphi,
+        psi=psi,
+        cos_inc=cos_inc,
+        log10_M=log10_M,
+        q=q,
+        log10_F=log10_F,
+        e0=e0,
+        gamma0=gamma0,
+        l0=l0,
+        tref=tref,
+        z=z,
+        psrTerm=psrTerm,
+    )
+
     # I am setting ecc=False to avoid eccess parameters.
     ecw = CWSignal(wf, ecc=False, psrTerm=psrTerm, name=name)
 
     return ecw
-
--- enterprise_GWecc/enterprise_GWecc.py
+++ enterprise_GWecc/enterprise_GWecc.py
@@ -1,146 +1,182 @@
 from enterprise_GWecc.GWecc import EccentricResiduals
-from enterprise_GWecc.GWecc import ResidualsMethod_Num, ResidualsTerms_Both, ResidualsTerms_Earth
+from enterprise_GWecc.GWecc import (
+    ResidualsMethod_Num,
+    ResidualsTerms_Both,
+    ResidualsTerms_Earth,
+)
 from enterprise_GWecc.GWecc import FeStatFuncs
 from enterprise.signals import signal_base
 import numpy as np
 
-year_to_s   = 365.25*24*3600
+year_to_s = 365.25 * 24 * 3600
+
 
 @signal_base.function
-def eccentric_cw_delay(toas, 
-                       theta, phi, pdist, 
-                       cos_gwtheta, gwphi, 
-                       log10_dist,
-                       log10_h,
-                       psi, cos_inc,
-                       log10_M, q,
-                       log10_F, e0, gamma0, l0, tref,
-                       z,
-                       p_dist=1.0,
-                       psrTerm=False,
-                       evolve=True):
+def eccentric_cw_delay(
+    toas,
+    theta,
+    phi,
+    pdist,
+    cos_gwtheta,
+    gwphi,
+    log10_dist,
+    log10_h,
+    psi,
+    cos_inc,
+    log10_M,
+    q,
+    log10_F,
+    e0,
+    gamma0,
+    l0,
+    tref,
+    z,
+    p_dist=1.0,
+    psrTerm=False,
+    evolve=True,
+):
     """
     ======================================================================================================================
     PARAM       DESCRIPTION                             UNIT        EXPRESSION              COMMENTS
     ======================================================================================================================
     toas        are Pulsar TOAs                         in s                                (Taken from the Pulsar object)
-    
-    theta       is  Polar angle of pulsar               in rad      = pi/2 - DEC_psr        (Taken from the Pulsar object)      
+
+    theta       is  Polar angle of pulsar               in rad      = pi/2 - DEC_psr        (Taken from the Pulsar object)
     phi         is  Azimuthal angle of pulsar           in rad      = RA_psr                (Taken from the Pulsar object)
     pdist       is  distance to pulsar (value,error)    in kpc                              (Taken from the Pulsar object)
-    
+
     p_dist      is  Pulsar distance prior               in kpc                              (Parametrizes the departure from the measured pulsar distance given in pdist.)
-    
-    cos_gwtheta is  Cos of Polar angle of GW source     in rad      = cos(pi/2 - DEC_GW)     
+
+    cos_gwtheta is  Cos of Polar angle of GW source     in rad      = cos(pi/2 - DEC_GW)
     gwphi       is  Azimithal angle of GW source        in rad      = RA_GW
-    
-    log10_dist  is  log10 distance to GW source         in Mpc      = log10(D_GW)           (One and only one of log10_dist and log10_h should be used.) 
-    
-    log10_h     is  log10 GW amplitude at tref                      = log10(H0)             (One and only one of log10_dist and log10_h should be used.) 
-    
-    psi         is  Polarization angle                  in rad      
+
+    log10_dist  is  log10 distance to GW source         in Mpc      = log10(D_GW)           (One and only one of log10_dist and log10_h should be used.)
+
+    log10_h     is  log10 GW amplitude at tref                      = log10(H0)             (One and only one of log10_dist and log10_h should be used.)
+
+    psi         is  Polarization angle                  in rad
     cos_inc     is  Cos of Inclination of GW source     in rad      = cos(i)
-    
+
     log10_M     is  log10 Total Mass of GW src          in MSun     = log10(M)              (Not to be confused with the chirp mass.)
     q           is  Mass ratio of GW source                         = m1/m2
-    
+
     log10_F     is  log10 GW frequency at t=tref        in Hz       = log10(2/Pb0)
     e0          is  Eccentricity at t=tref
     gamma0      is  Periastron angle at t=tref          in rad
     l0          is  Mean anomaly at t=tref              in rad                              (Should be set to zero if tref is a free parameter.)
     tref        is  Reference time                      in s                                (Should be set to a fixed epoch if l0 is a free parameter.)
-    
+
     z           is  redshift
-    
-    psrTerm     is  [boolean] Whether to add pulsar term 
-       
+
+    psrTerm     is  [boolean] Whether to add pulsar term
+
     evolve      is  [boolean] Whether to evolve phase exactly                               Not implemented (Always True)
-    
+
     ======================================================================================================================
-    
-    Returns:    
+
+    Returns:
             TOA delays due to GWs from eccentric binary sources (in s)
     """
-    
-    toas = toas / (24*3600)
-    tref = tref / (24*3600)
-    
-    # Check the precision of these parameters later. 
-    RA_psr  = phi
-    DEC_psr = np.pi/2 - theta
-    
-    #converts from kpc to pc
+
+    toas = toas / (24 * 3600)
+    tref = tref / (24 * 3600)
+
+    # Check the precision of these parameters later.
+    RA_psr = phi
+    DEC_psr = np.pi / 2 - theta
+
+    # converts from kpc to pc
     if type(pdist) is not tuple:
-        D_psr = pdist*1000 
+        D_psr = pdist * 1000
     else:
-        D_psr = (pdist[0]+pdist[1]*p_dist)*1000 
-    
+        D_psr = (pdist[0] + pdist[1] * p_dist) * 1000
+
     gwtheta = np.arccos(cos_gwtheta)
-    DEC_GW = np.pi/2 - gwtheta
-    RA_GW  = gwphi
-        
-    M = 10.**log10_M
-    
+    DEC_GW = np.pi / 2 - gwtheta
+    RA_GW = gwphi
+
+    M = 10.0**log10_M
+
     i = np.arccos(cos_inc)
-    
-    n0 = np.pi*(10.**log10_F)    # GW frequency is twice the orbital frequency.
-    Pb0 = 2*np.pi/n0 / year_to_s
-    
-    if (log10_dist is None and log10_h is None) or (log10_dist is not None and log10_h is not None):
-        raise ValueError("One and only one of log10_dist and log10_h should be used. The other should be set to None.")
+
+    n0 = np.pi * (10.0**log10_F)  # GW frequency is twice the orbital frequency.
+    Pb0 = 2 * np.pi / n0 / year_to_s
+
+    if (log10_dist is None and log10_h is None) or (
+        log10_dist is not None and log10_h is not None
+    ):
+        raise ValueError(
+            "One and only one of log10_dist and log10_h should be used. The other should be set to None."
+        )
     elif log10_dist is not None:
-        D_GW = 1e6 * (10.**log10_dist)
+        D_GW = 1e6 * (10.0**log10_dist)
     else:
-        H0 = 10.**log10_h
-        x0 = (M*n0)**(2/3)
-        eta = q/(1+q)**2
-        D_GW = M*eta*x0/H0
+        H0 = 10.0**log10_h
+        x0 = (M * n0) ** (2 / 3)
+        eta = q / (1 + q) ** 2
+        D_GW = M * eta * x0 / H0
 
     residuals_method = ResidualsMethod_Num
     residuals_terms = ResidualsTerms_Both if psrTerm else ResidualsTerms_Earth
-      
+
     return np.asarray(
-               EccentricResiduals(M, q,
-                                  psi, i,
-                                  tref, Pb0, e0, l0, gamma0,
-                                  D_GW, RA_GW, DEC_GW,
-                                  D_psr,  RA_psr,  DEC_psr,
-                                  z,
-                                  residuals_method,
-                                  residuals_terms,
-                                  toas)    
-            )
+        EccentricResiduals(
+            M,
+            q,
+            psi,
+            i,
+            tref,
+            Pb0,
+            e0,
+            l0,
+            gamma0,
+            D_GW,
+            RA_GW,
+            DEC_GW,
+            D_psr,
+            RA_psr,
+            DEC_psr,
+            z,
+            residuals_method,
+            residuals_terms,
+            toas,
+        )
+    )
+
 
 @signal_base.function
-def Fe_statistic_funcs(toas, 
-                       theta, phi, 
-                       cos_gwtheta, gwphi,
-                       log10_dist,
-                       log10_M, q,
-                       log10_F, e0, l0, tref,
-                       z):
+def Fe_statistic_funcs(
+    toas,
+    theta,
+    phi,
+    cos_gwtheta,
+    gwphi,
+    log10_dist,
+    log10_M,
+    q,
+    log10_F,
+    e0,
+    l0,
+    tref,
+    z,
+):
 
-    M = 10.**log10_M
-    
-    toas = toas / (24*3600)
-    tref = tref / (24*3600)
-    
-    RA_P  = phi
-    DEC_P = np.pi/2 - theta
-    
+    M = 10.0**log10_M
+
+    toas = toas / (24 * 3600)
+    tref = tref / (24 * 3600)
+
+    RA_P = phi
+    DEC_P = np.pi / 2 - theta
+
     gwtheta = np.arccos(cos_gwtheta)
-    DEC_GW = np.pi/2 - gwtheta
-    RA_GW  = gwphi
-    D_GW = 1e6 * (10.**log10_dist)
-    
-    n0 = np.pi*(10.**log10_F)    # GW frequency is twice the orbital frequency.
-    Pb0 = 2*np.pi/n0 / year_to_s
+    DEC_GW = np.pi / 2 - gwtheta
+    RA_GW = gwphi
+    D_GW = 1e6 * (10.0**log10_dist)
+
+    n0 = np.pi * (10.0**log10_F)  # GW frequency is twice the orbital frequency.
+    Pb0 = 2 * np.pi / n0 / year_to_s
 
     return np.asarray(
-                FeStatFuncs(M, q, tref, Pb0, e0, l0,
-                            D_GW, RA_GW, DEC_GW, 
-                            RA_P, DEC_P, 
-                            z,
-                            toas)
-           )
-
+        FeStatFuncs(M, q, tref, Pb0, e0, l0, D_GW, RA_GW, DEC_GW, RA_P, DEC_P, z, toas)
+    )
