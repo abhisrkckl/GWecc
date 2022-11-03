fd941833c8e18fe629a11087d5523721753daba5
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-11-03 14:20:23 -0500

    moved test_EccentricResiduals_px

--- tests/test_EccentricResiduals.py
+++ tests/test_EccentricResiduals.py
@@ -3,6 +3,7 @@ import numpy as np
 from itertools import product as outer_product
 from enterprise_GWecc.GWecc import (
     EccentricResiduals,
+    EccentricResiduals_px,
     ResidualsMethod_Num,
     ResidualsMethod_Adb,
     ResidualsMethod_Anl,
@@ -47,3 +48,30 @@ def test_EccentricResiduals(method, term):
     )
 
     assert np.all(np.isfinite(res)) and not np.all(res == 0) and len(res) == ntoas
+
+@pytest.mark.parametrize("method, term", outer_product(methods, terms))
+def test_EccentricResiduals_px(method, term):
+
+    delay = -1000  # -D_P*(1-cosmu) / (1+z)
+
+    res_p, res_x = EccentricResiduals_px(
+        M,
+        q,
+        Omega,
+        i,
+        t0,
+        Pb0,
+        e0,
+        l0,
+        gamma0,
+        D_GW,
+        delay,
+        z,
+        method,
+        term,
+        toas,
+    )
+
+    assert np.all(np.isfinite(res_p)) and np.all(np.isfinite(res_x))
+    assert not np.all(res_p == 0) and not np.all(res_x == 0)
+    assert len(res_p) == ntoas and len(res_x) == ntoas
