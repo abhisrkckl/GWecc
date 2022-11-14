52c506a0332eae6da65497b8c5895204cda91b50
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-11-14 14:51:52 -0600

    AntennaPattern struct

--- GWecc/AntennaPattern.cpp
+++ GWecc/AntennaPattern.cpp
@@ -1,10 +1,9 @@
-
 #include "AntennaPattern.hpp"
 #include <Eigen/Dense>
 #include <cmath>
 #include "ipow.hpp"
 
-std::tuple<double,double,double> antenna_pattern(const SkyPosition &bin_pos, const SkyPosition &psr_pos){
+AntennaPattern antenna_pattern(const SkyPosition &bin_pos, const SkyPosition &psr_pos){
     
     const double &lambda_p = psr_pos.RA,
                  &beta_p   = psr_pos.DEC,
@@ -16,11 +15,11 @@ std::tuple<double,double,double> antenna_pattern(const SkyPosition &bin_pos, con
                  n2 = sin(lambda_p)*cos(beta_p),
                  n3 = sin(beta_p);
     
-    const double cos_theta = cos(beta)*cos(beta_p)*cos(lambda-lambda_p) + sin(beta)*sin(beta_p);
+    const double cosmu = cos(beta)*cos(beta_p)*cos(lambda-lambda_p) + sin(beta)*sin(beta_p);
     
     
     double Fp = 0, Fx = 0;
-    if(cos_theta!=1){
+    if(cosmu!=1){
         const double    e11p = ipow(sin(lambda),2)-ipow(cos(lambda),2)*ipow(sin(beta),2),
                         e21p = -sin(lambda)*cos(lambda)*(ipow(sin(beta),2)+1),
                         e31p = cos(lambda)*sin(beta)*cos(beta),
@@ -34,7 +33,7 @@ std::tuple<double,double,double> antenna_pattern(const SkyPosition &bin_pos, con
         Fp = (n1*(n1*e11p+n2*e12p+n3*e13p)+
               n2*(n1*e21p+n2*e22p+n3*e23p)+
               n3*(n1*e31p+n2*e32p+n3*e33p));
-        Fp *= 0.5 / (1-cos_theta);
+        Fp *= 0.5 / (1-cosmu);
         
         /************/
         
@@ -51,9 +50,9 @@ std::tuple<double,double,double> antenna_pattern(const SkyPosition &bin_pos, con
         Fx = (n1*(n1*e11c+n2*e12c+n3*e13c)+
               n2*(n1*e21c+n2*e22c+n3*e23c)+
               n3*(n1*e31c+n2*e32c+n3*e33c));
-        Fx *= 0.5 / (1-cos_theta);
+        Fx *= 0.5 / (1-cosmu);
         
     }
     
-    return std::make_tuple(cos_theta, Fp, Fx);
+    return {cosmu, Fp, Fx};
 }
--- GWecc/AntennaPattern.hpp
+++ GWecc/AntennaPattern.hpp
@@ -4,10 +4,10 @@
 #include <tuple>
 #include "Binary.hpp"
 
-/*
- * Returns:
- *	[cos(eta), Fp, Fx]
- */
-std::tuple<double,double,double> antenna_pattern(const SkyPosition &bin_pos, const SkyPosition &psr_pos);
+struct AntennaPattern{
+    double cosmu, Fp, Fx;
+};
+
+AntennaPattern antenna_pattern(const SkyPosition &bin_pos, const SkyPosition &psr_pos);
 
 #endif
